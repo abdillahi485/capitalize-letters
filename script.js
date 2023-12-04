72988201b31e957a851318b6da47dff068db13eb
Abdillahi485 <daakircadde143@gmail.com>
2023-12-04 19:43:47 +0300

    finished capitalize-letters

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+function makeCapital(str){
+    let arr=str.toLowerCase().split(' ')
+    let capitalizedStr=arr.map((val)=>{
+        return val.replace(val.charAt(0),val.charAt(0).toUpperCase())
+    })
+    return capitalizedStr
+}
+
+let result1=makeCapital("my name is ali") 
+let result2=makeCapital("fikrcamp Bootcamp")
+let result3=makeCapital("i live In Paris")
+console.log(result1)
+console.log(result2)
+console.log(result3)
